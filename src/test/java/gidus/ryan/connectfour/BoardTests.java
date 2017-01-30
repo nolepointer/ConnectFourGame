0f236a1726ff26f2cf9cec3d8240fde2fc1cbd70
Ryan <ryan.gidus@gmail.com>
2017-01-29 21:30:07 -0500

    Added more testing

--- src/test/java/gidus/ryan/connectfour/BoardTests.java
+++ src/test/java/gidus/ryan/connectfour/BoardTests.java
@@ -7,11 +7,43 @@ import org.junit.Test;
 public class BoardTests {
 	
 	@Test
+	public void testInitializeBoard() {
+		GamePiece human = GamePiece.BLACK;
+		Board board = new Board(human);
+		
+		assertEquals(board.getHumanGamePiece(), GamePiece.BLACK);
+		assertFalse(board.isWinner());
+		assertEquals(board.getNumberOfMoves(), 0);
+		assertEquals(board.getComputerGamePiece(), GamePiece.RED);
+		assertEquals(board.getStatus(), Status.ONGOING);
+
+		GamePiece[][] gameBoard = board.getGameBoard();
+		
+		for(int i = 0; i < Board.NUM_ROWS; i++) {
+			for(int j = 0; j < Board.NUM_COLUMNS; j++) {
+				assertEquals(gameBoard[i][j], GamePiece.EMPTY);
+			}
+		}
+	
+	}
+	
+	@Test
+	public void testDeclareDraw(){
+		GamePiece human = GamePiece.BLACK;
+		Board board = new Board(human);
+		board.declareDraw();
+		
+		assertFalse(board.isWinner());
+		assertEquals(board.getStatus(), Status.DRAW);
+	}
+	
+	@Test
 	public void testDeclareWinnerHuman(){
 		GamePiece human = GamePiece.BLACK;
 		Board board = new Board(human);
 		board.declareWinner(human);
 		
+		assertEquals(board.getStatus(), Status.OVER);
 		assertTrue(board.isWinner());
 	}
 	
@@ -21,6 +53,7 @@ public class BoardTests {
 		Board board = new Board(human);
 		board.declareWinner(GamePiece.RED);
 		
+		assertEquals(board.getStatus(), Status.OVER);
 		assertFalse(board.isWinner());
 	}
 
